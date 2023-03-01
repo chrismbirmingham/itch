86af3c584894f11920bb9f13ac1cd49e9d1bf679
Braden McDorman <bmcdorman@gmail.com>
2023-03-01 20:23:18 +0000

    fix rest of variable methods

--- src/module/data.ts
+++ src/module/data.ts
@@ -9,7 +9,7 @@ export default {
   showlist: (ctx: DispatchContext) => ctx.show(ctx, ctx.getField('LIST').value as string),
   hidelist: (ctx: DispatchContext) => ctx.hide(ctx, ctx.getField('LIST').value as string),
   itemoflist: (ctx: DispatchContext) => {
-    const list = ctx.resolveField('LIST');
+    const list = ctx.heap.get(ctx.getField('LIST').value as string);
     const index = toNumber(ctx.resolveValue('INDEX'));
 
     if (typeof index !== 'number') throw new Error('INDEX must be number');
@@ -18,30 +18,30 @@ export default {
     return list[index];
   },
   lengthoflist: (ctx: DispatchContext) => {
-    const list = ctx.resolveField('LIST');
+    const list = ctx.heap.get(ctx.getField('LIST').value as string)
     if (!Array.isArray(list)) throw new Error('LIST must be array');
     return list.length;
   },
   itemnumoflist: (ctx: DispatchContext) => {
-    const list = ctx.resolveField('LIST');
+    const list = ctx.heap.get(ctx.getField('LIST').value as string)
 
     if (!Array.isArray(list)) throw new Error('LIST must be array');
 
     return list.indexOf(ctx.resolveValue('ITEM'));
   },
   addtolist: (ctx: DispatchContext) => {
-    const list = ctx.resolveField('LIST');
+    const list = ctx.heap.get(ctx.getField('LIST').value as string)
 
     if (!Array.isArray(list)) throw new Error('LIST must be array');
 
     list.push(ctx.resolveValue('ITEM'));
   },
   deletealloflist: (ctx: DispatchContext) => {
-    const list = ctx.resolveField('LIST');
+    const list = ctx.heap.get(ctx.getField('LIST').value as string)
     if (Array.isArray(list)) list.length = 0;
   },
   deleteoflist: (ctx: DispatchContext) => {
-    const list = ctx.resolveField('LIST');
+    const list = ctx.heap.get(ctx.getField('LIST').value as string)
     const index = toNumber(ctx.resolveValue('INDEX'));
 
     if (typeof index !== 'number') throw new Error('INDEX must be number');
@@ -51,7 +51,7 @@ export default {
     list.splice(index, 1);
   },
   insertatlist: (ctx: DispatchContext) => {
-    const list = ctx.resolveField('LIST');
+    const list = ctx.heap.get(ctx.getField('LIST').value as string)
     const index = toNumber(ctx.resolveValue('INDEX'));
     const item = ctx.resolveValue('ITEM');
 
@@ -67,7 +67,7 @@ export default {
 
     if (typeof value !== 'number') throw new Error('VALUE must be number');
 
-    const currentValue = ctx.heap.get(variable.name);
+    const currentValue = ctx.heap.get(variable.value as string);
     if (typeof currentValue !== 'number') throw new Error('VARIABLE must be number')
 
     ctx.heap.set(variable.name, currentValue + value);
@@ -76,6 +76,6 @@ export default {
     const variable = ctx.getField('VARIABLE');
     const value = ctx.resolveValue('VALUE');
 
-    ctx.heap.set(variable.name, value);
+    ctx.heap.set(variable.value as string, value);
   }
 } as Module;
